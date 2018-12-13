174cd1be4e91038200a7fb8cd2f9b974068bef9f
Gregory Brooks <gregox273@gmail.com>
2018-12-13 12:53:09 +0000

    Rename arduino sketch and remove .vs directory

--- backend_test/backend_test.ino
+++ backend_test/backend_test.ino
@@ -1,3 +1,6 @@
+#define METADATALEN 8
+#define BUFLEN 120
+
 void setup() {
 
   Serial.begin(57600);
@@ -14,21 +17,21 @@ void loop() {
   uint8_t ID = 0b01000101;
   uint32_t timestamp = 6787;
 
-  uint8_t meta_data[8];
+  uint8_t meta_data[METADATALEN];
   meta_data[0] = ID;
   meta_data[4] = (uint8_t)(timestamp & 0xFF);
   meta_data[5] = (uint8_t)((timestamp >> 8) & 0xFF);
   meta_data[6] = (uint8_t)((timestamp >> 16) & 0xFF);
   meta_data[7] = (uint8_t)((timestamp >> 24) & 0xFF);
   
-  uint64_t Data = 450648;
-  uint8_t buffer_data[8];
+  uint8_t buffer_data[BUFLEN];
 
-  for(int i = 0; i <8; i++){
-    buffer_data[i] = (uint8_t)(Data >> 8*i) & 0xFF;
+  for(int i = 0; i < BUFLEN; i++){
+    buffer_data[i] = i & 0xFF;
   }
-  
-  
-  Serial.write(meta_data, 4);
-  Serial.write(buffer_data, 4);
+    
+  Serial.write(meta_data, sizeof(meta_data));
+  Serial.write(buffer_data, sizeof(buffer_data));
+
+  delay(1000);
 }
